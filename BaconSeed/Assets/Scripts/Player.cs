b48d6121380666a7ba925107bccc530904f282c6
Goffin <tomajster@gmail.com>
2015-08-03 23:42:31 +0200

    Prawie poruszanie sie

--- BaconSeed/Assets/Scripts/Player.cs
+++ BaconSeed/Assets/Scripts/Player.cs
@@ -1,22 +1,62 @@
-﻿using UnityEngine;
+﻿using System.Linq;
+using UnityEngine;
 using System.Collections;
 
-public class Player : MonoBehaviour {
+public class Player : MonoBehaviour
+{
 
     public Rigidbody2D rb;
-	// Use this for initialization
-	void Start () {
+    public KeyCode JumpKey;
+    public KeyCode LeftKey;
+    public KeyCode RightKey;
+    public float JumpPower;
+    public float HorizontalPower;
+    public float HorizontalDragFactor;
+    public GameObject[] TerrainCheckers;
+    // Use this for initialization
+    void Start()
+    {
         rb = GetComponent<Rigidbody2D>();
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+        //JumpPower = 10.0f;
+        //HorizontalPower = 10.0f;
+        //HorizontalDragFactor = 1.01f;
+        JumpKey = KeyCode.UpArrow;
+        LeftKey = KeyCode.LeftArrow;
+        RightKey = KeyCode.RightArrow;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
     void FixedUpdate()
     {
-        if (Input.GetButtonDown("up"))
-            rb.velocity = new Vector3(0, 10, 0);
+        bool touchingGround = TerrainCheckers.Any(o => Physics2D.OverlapPoint(o.transform.position, LayerMask.GetMask("Ground") | LayerMask.GetMask("Player")));
+        if (touchingGround)
+        {
+            Vector3 CurreState = rb.velocity;
+            if (Input.GetKey(JumpKey))
+            {
+                Vector3 Jump = new Vector3(0, JumpPower, 0);
+                rb.velocity = CurreState + Jump;
+            }
+            if (Input.GetKey(LeftKey))
+            {
+                Vector3 Left = new Vector3(-HorizontalPower, 0, 0);
+                rb.velocity = CurreState + Left;
+            }
+            if (Input.GetKey(RightKey))
+            {
+                Vector3 Right = new Vector3(HorizontalPower, 0, 0);
+                rb.velocity = CurreState + Right;
+            }
+            rb.velocity = new Vector3(rb.velocity.x / HorizontalDragFactor, rb.velocity.y, 0);
+            
+        }
+    }
+    void OnCollision()
+    {
 
     }
 }
