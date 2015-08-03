c6819cfa46da005bc116bf2054159f24a5c1bb79
Goffin <tomajster@gmail.com>
2015-08-03 20:58:39 +0200

    Initial commit2 :P

---
+++ BaconSeed/Assets/Scripts/Player.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+    public Rigidbody2D rb;
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+    void FixedUpdate()
+    {
+        if (Input.GetButtonDown("up"))
+            rb.velocity = new Vector3(0, 10, 0);
+
+    }
+}
